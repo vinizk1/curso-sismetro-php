c85cc105e921973a490d3a7056ac95d3f63542d0
Vinicius <vinizarkin@gmail.com>
2023-11-09 15:49:39 -0300

    aula 22

--- Aula 21-2/formulariofibonnaci.php
+++ Aula 21-2/formulariofibonnaci.php
@@ -1,3 +1,25 @@
+<?php
+
+function fibonacci($p)
+{
+    $sequencia = array();
+    $sequencia[0] = 1;
+    $sequencia[1] = 1;
+
+    for ($i = 2; $i < $p; $i++) {
+        $sequencia[$i] = $sequencia[$i - 1] + $sequencia[$i - 2];
+    }
+
+    return $sequencia[$p - 1];
+}
+
+?>
+
+
+
+
+
+
 <!doctype html>
 <html lang="pt-br">
 
@@ -5,29 +27,77 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Aula 21</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 
-<body style="background: rgb(131,58,180);
-background: linear-gradient(90deg, rgba(131,58,180,1) 0%, rgba(253,29,29,1) 50%, rgba(252,176,69,1) 100%);">
+<body style="background: rgb(26,255,0);
+background: linear-gradient(90deg, rgba(26,255,0,1) 15%, rgba(255,249,0,1) 53%, rgba(221,9,80,1) 100%);">
+    </br>
+    </br>
+    </br>
+    </br>
+    </br>
+    </br>
+    </br>
     <h1 style="text-align: center;">Bão dia</h1>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
-        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 
     <div class="row">
         <div class="col-4 offset-4">
-            <form method="POST" action="codphp.php">
-            <div class="mb-3">
-                <label for="exampleFormControlInput1" class="form-label">Posição</label>
-                <input type="name" class="form-control" id="exampleFormControlInput1" name="posicao" placeholder="Posição da sequência de fibonacci">
-            </div>
 
-            <button type="submit" class="btn btn-success">Enviar!</button>
-        </form>
+
+            
+
+                <h2 style="text-align: center;">RESULTADO</h2>
+                <div class="row">
+                    <div class="col-2 offset-3 text-center">
+                        <p>Posição
+                        </p>
+                    </div>
+                    <div class="col-5 text-center">
+                        <p>Valor
+                        </p>
+                    </div>
+                </div>
+                
+                <?php
+            if (isset($_POST["posicao"])) {
+
+
+
+            ?>
+                <div class="row">
+                    <div class="col-2 offset-3 text-center">
+                        <p><?php echo $_POST["posicao"]; ?>  </p>
+                    </div>
+                    <div class="col-5 text-center">
+                        <p><?php echo fibonacci($_POST["posicao"]); ?>  </p>
+                    </div>
+                </div>
+
+                <?php
+            }
+?>
+
+                <form method="POST">
+
+                    <form method="POST">
+                        <div class="mb-3">
+                            <label for="exampleFormControlInput1" class="form-label" style="color: white">Posição</label>
+                            <input type="name" class="form-control" id="exampleFormControlInput1" name="posicao" placeholder="Posição da sequência de fibonacci">
+                        </div>
+                        <div class="col-4 offset 6">
+                            <button type="submit" class="btn btn-success">Enviar!</button>
+                        </div>
+                    </form>
         </div>
     </div>
+
+
+
+
+
+
 
 
 
